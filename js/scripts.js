03727bd4e50750ccd5f07cbe99712abf429f9747
Dustin Herboldshimer and Nathan Aden <dustin@>
2019-03-27 10:26:26 -0700

    add bootstrap.min.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,42 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+  $("#address-form").submit(function(event) {
     var name = $("#form-name").val();
     var address = $("#form-address").val();
     var city = $("#form-city").val();
     var state = $("#inputState").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
-    $("#story").show();
+    console.log(name);
+    $("#name").text(name);
+    $("#address").text(address);
+    $("#city").text(city);
+    $("#state").text(state);
 
     event.preventDefault();
   });
 
+  // $('#myModal').on('shown.bs.modal', function () {
+  //   $('#myInput').trigger('focus')
+  // })
 
-  $("#address-form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
-    $("#story").show();
-
-    event.preventDefault();
-  });
+  // $("#address-form").submit(function(event) {
+  //   var person1Input = $("input#person1").val();
+  //   var person2Input = $("input#person2").val();
+  //   var animalInput= $("input#animal").val();
+  //   var exclamationInput = $("input#exclamation").val();
+  //   var verbInput = $("input#verb").val();
+  //   var nounInput = $("input#noun").val();
+  //
+  //   $(".person1").text(person1Input);
+  //   $(".person2").text(person2Input);
+  //   $(".animal").text(animalInput);
+  //   $(".exclamation").text(exclamationInput);
+  //   $(".verb").text(verbInput);
+  //   $(".noun").text(nounInput);
+  //
+  //   $("#story").show();
+  //
+  //   event.preventDefault();
+  // });
 
 
 
